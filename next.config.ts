619d9ed77e6db5c50fb5de783b8f96b0a6336c04
田村和也 <tamtam.kt7@gmail.com>
2026-07-03 10:03:19 +0900

    fix: resolve firebase-admin ERR_REQUIRE_ESM by externalizing package and overriding jose version

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['firebase-admin'],
 };
 
 export default withSentryConfig(nextConfig, {
